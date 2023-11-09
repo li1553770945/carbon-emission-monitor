a7c38e4d4a513ab17447aefb4c0efa92c6e290c6
li1553770945 <1553770945@qq.com>
2023-11-09 10:27:43 +0800

    feat(user):实现用户的数据库链接

---
+++ src/main/java/com/main/carbon_emission_monitor/service/IPhotovoltaicService.java
@@ -0,0 +1,7 @@
+package com.main.carbon_emission_monitor.service;
+
+import com.main.carbon_emission_monitor.controller.ElectricityController;
+
+public interface IPhotovoltaicService {
+    void ElectricityData();
+}
---
+++ src/main/java/com/main/carbon_emission_monitor/service/impl/PhotovoltaicService.java
@@ -0,0 +1,10 @@
+package com.main.carbon_emission_monitor.service.impl;
+
+import com.main.carbon_emission_monitor.dto.water.MeteringResponse;
+import com.main.carbon_emission_monitor.service.IPhotovoltaicService;
+
+public class PhotovoltaicService implements IPhotovoltaicService {
+    public void  ElectricityData(){
+        return;
+    }
+}
